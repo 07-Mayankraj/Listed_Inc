69e6cc6f66856402b730163383848ec7c6d0154f
07.Mayank-raj <2003.mayank.raj@gmail.com>
2023-05-24 19:57:09 +0530

    added auto email check interval

--- index.js
+++ index.js
@@ -114,6 +114,63 @@ const getRandomInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// calling funtion at specified interval
+setInterval(() => {
+    app.get("/auth/callback", async (req, res) => {
+        // authorization code
+        const code = req.query.code;
+        // console.log({code})
+        try {
+            // access and refresh tokens
+            const { tokens } = await oauth2Client.getToken(code);
+            console.log({ tokens });
+            // Setting the credentials
+            oauth2Client.setCredentials(tokens);
+            // Gmail API client
+            const gmail = google.gmail({ version: "v1", auth: oauth2Client });
+            console.log({ gmail });
+            // Retrieve email address
+            const profile = await gmail.users.getProfile({ userId: "me" });
+            console.log({ profile });
+
+            // check new email 
+
+
+            const checkNewEmails = async () => {
+                const res = await gmail.users.messages.list({ userId: "me" });
+                const emails = res.data.messages || [];
+
+                for (const email of emails) {
+                    const msg = await gmail.users.messages.get({ userId: "me", id: email.id });
+                    const headers = msg.data.payload.headers;
+                    const from = headers.find((header) => header.name === "From").value;
+                    const replies = headers.filter(
+                        (header) => header.name === "In-Reply-To" || header.name === "References"
+                    );
+                    //   console.log(msg)
+                    const hasPriorReplies = replies.length > 0;
+                    //!    this code will start sending email to unread emails 
+                    // if (!hasPriorReplies) {
+                    //     await sendAutoReply({ from });
+                    // }
+                }
+            };
+
+            //   calling checkin
+            checkNewEmails();
+
+            res.send(`<h1>email address :${profile.data.emailAddress} </h1>
+                    <h1>Total messages:${profile.data.messagesTotal} </h1>
+                    <h1>Total Threads: ${profile.data.threadsTotal}</h1>`
+            );
+
+
+        } catch (error) {
+            console.error("Error retrieving access token:", error);
+            res.status(500).send("Error retrieving access token.");
+        }
+    });
+}, getRandomInterval(40, 120) * 1000)
 
 
 
